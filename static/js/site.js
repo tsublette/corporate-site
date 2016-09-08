039283bfe8082af9dffef3fefcc6fa5222e2f81b
Ryan Walls <ryan.walls@3dsim.com>
2016-09-08 15:10:20 -0600

    Add subscribe modal with working form

--- static/js/site.js
+++ static/js/site.js
@@ -16,6 +16,38 @@ $(document).ready(function() {
         typeNextString();
       }
   });
+  $("input[type='checkbox']").change(function(){
+    var numCheckedCheckboxesOnForm = $(this).parents('form').children("input[type='checkbox']:checked").length;
+    var numCheckboxesOnForm = $(this).parents('form').children("input[type='checkbox']").length;
+    if (numCheckedCheckboxesOnForm === numCheckboxesOnForm) {
+       console.log("all checkboxes checked");
+       if ($(this).siblings("input[name=planFrequency]:checked").val() === "monthly") {
+         $(this).siblings("#monthlyPayButton").show();
+         $(this).siblings("#yearlyPayButton").hide();
+       } else {
+         $(this).siblings("#monthlyPayButton").hide();
+         $(this).siblings("#yearlyPayButton").show();
+       }
+    } else {
+      console.log("some checkboxes unchecked");
+      $(this).siblings("#monthlyPayButton").hide();
+      $(this).siblings("#yearlyPayButton").hide();
+    }
+  });
+  $("input[type='radio']").change(function(){
+    var numCheckedCheckboxesOnForm = $(this).parents('form').children("input[type='checkbox']:checked").length;
+    var numCheckboxesOnForm = $(this).parents('form').children("input[type='checkbox']").length;
+    if (numCheckedCheckboxesOnForm === numCheckboxesOnForm) {
+       if ($(this).val() === "monthly") {
+         $(this).siblings("#monthlyPayButton").show();
+         $(this).siblings("#yearlyPayButton").hide();
+       } else {
+         $(this).siblings("#monthlyPayButton").hide();
+         $(this).siblings("#yearlyPayButton").show();
+       }
+    }
+  });
+
 });
 
 function typeNextString() {
